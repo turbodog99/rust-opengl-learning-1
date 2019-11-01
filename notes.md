efcb03875b5d0a7da48bea6cbf5f3f5432b93751
Mark Herman, II <mark@exceptionaltech.com>
2019-11-01 15:00:19 -0500

    Finished with walkthrough...moving on to my stuff

---
+++ notes.md
@@ -0,0 +1,50 @@
+Following along with: [Rust and OpenGL from scratch](https://nercury.github.io/rust/opengl/tutorial/2018/02/08/opengl-in-rust-from-scratch-00-setup.html)
+
+The syntax apparently changed on use declarations in Rust 2018. I ended up needing to refer to
+resources using super::resources::Resources in render_gl.rs.
+
+Build dependencies are separate. I slipped up on this initially and put the walkdir crate, which the
+build script needs, in the dependencies section.
+
+The example code had me create a variable called gl_context. In C, you'd normally save the
+context so that you can delete it later with SDL_GL_DeleteContext(gl_context). I was getting compiler
+errors about never using the gl_context variable. I temporarily deleted the line that creates it and
+stuff broke. Then, I tried not saving the variable since it wasn't used. Stuff was still broken.
+
+Coming from other languages, I had this instinct to ask, "why would I save this if I don't use it?"
+Well, it turns out having the variable there unused established its lifetime so the context could be
+cleaned up automatically when I was done with it.
+
+I chose to prepend the variable name with an _ so the compiler would quit complaining about it.
+
+I'm having some issues with local namespacing. Having to use super::whatever to get to a module in a
+source file in the same directory doesn't quite feel right. I might revisit it for further study.
+
+Use the repr macro to take control of the memory layout of a struct.
+
+At the end of [The page on refactoring the Vertex attributes](https://nercury.github.io/rust/opengl/tutorial/2018/06/27/opengl-in-rust-from-scratch-09-vertex-attribute-format.html),
+the author discusses why he chose to make the wrapping function safe but the functions
+that call the unsafe GL functions unsafe. It seems very indicative of the Rust
+philosophy.
+
+Procedural macros seem like a really slick idea. They let generate code at compile time to handle multiple types.
+
+For now, I stuck with putting the shaders in an assets directory. The author of the post I've been following seems to have moved shaders into the root directory at some point.
+
+It's apparently not possible to create a generic structure that doesn't actually
+contain the generic data type. So, there's a ::std::marker::PhantomData<type> object
+that acts as a placeholder but is zero-sized so is no cost.
+
+_marker: ::std::marker::PhantomData<B>
+
+The refactor of ArrayBuffer into a generic Buffer Struct got us ElementArrayBuffers
+nearly for free.
+
+Visual Studio code, at least the extension I'm using, flags use of procedural macros
+to let you know it can't statically check them...interesting.
+
+After using the provided Triangle code, I ended up removing my data.rs and using the
+author's, which necessitated adding a couple of new dependencies. He implemented all of
+the various GL data formats.
+
+I'm now finished the walk through...time to make this do something myself.
--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,17 @@ extern crate failure;
 
 extern crate gl;
 extern crate sdl2;
+#[macro_use]
+extern crate render_gl_derive;
 
 pub mod render_gl;
 pub mod resources;
 
 use failure::err_msg;
-use render_gl::data;
 use resources::Resources;
 use std::path::Path;
 
-#[derive(Copy, Clone, Debug)]
-#[repr(C, packed)]
-struct Vertex {
-    pos: data::f32_f32_f32,
-    clr: data::f32_f32_f32,
-}
+mod triangle;
 
 fn main() {
     if let Err(e) = run() {
@@ -54,74 +50,6 @@ fn run() -> Result<(), failure::Error> {
 
     let res = Resources::from_relative_exe_path(Path::new("assets"))?;
 
-    let shader_program = render_gl::Program::from_res(&gl, &res, "shaders/triangle").unwrap();
-
-    shader_program.set_used();
-
-    let vertices: Vec<Vertex> = vec![
-        Vertex {
-            pos: (0.5, -0.5, 0.0).into(),
-            clr: (1.0, 0.0, 0.0).into(),
-        }, // bottom right
-        Vertex {
-            pos: (-0.5, -0.5, 0.0).into(),
-            clr: (0.0, 1.0, 0.0).into(),
-        }, // bottom left
-        Vertex {
-            pos: (0.0, 0.5, 0.0).into(),
-            clr: (0.0, 0.0, 1.0).into(),
-        }, // top
-    ];
-
-    let mut vbo: gl::types::GLuint = 0;
-    unsafe {
-        gl.GenBuffers(1, &mut vbo);
-    }
-
-    unsafe {
-        gl.BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl.BufferData(
-            gl::ARRAY_BUFFER,                                                          // target
-            (vertices.len() * std::mem::size_of::<Vertex>()) as gl::types::GLsizeiptr, // size of data in bytes
-            vertices.as_ptr() as *const gl::types::GLvoid, // pointer to data
-            gl::STATIC_DRAW,                               // usage
-        );
-        gl.BindBuffer(gl::ARRAY_BUFFER, 0); // unbind the buffer
-    }
-
-    let mut vao: gl::types::GLuint = 0;
-    unsafe {
-        gl.GenVertexArrays(1, &mut vao);
-    }
-
-    unsafe {
-        gl.BindVertexArray(vao);
-        gl.BindBuffer(gl::ARRAY_BUFFER, vbo);
-
-        gl.EnableVertexAttribArray(0); // this is "layout (location = 0)" in vertex shader
-        gl.VertexAttribPointer(
-            0,         // index of the generic vertex attribute ("layout (location = 0)")
-            3,         // the number of components per generic vertex attribute
-            gl::FLOAT, // data type
-            gl::FALSE, // normalized (int-to-float conversion)
-            (6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
-            std::ptr::null(),                                     // offset of the first component
-        );
-
-        gl.EnableVertexAttribArray(1); // this is "layout (location = 0)" in vertex shader
-        gl.VertexAttribPointer(
-            1,         // index of the generic vertex attribute ("layout (location = 0)")
-            3,         // the number of components per generic vertex attribute
-            gl::FLOAT, // data type
-            gl::FALSE, // normalized (int-to-float conversion)
-            (6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
-            (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid, // offset of the first component
-        );
-
-        gl.BindBuffer(gl::ARRAY_BUFFER, 0);
-        gl.BindVertexArray(0);
-    }
-
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
         for event in event_pump.poll_iter() {
@@ -136,15 +64,8 @@ fn run() -> Result<(), failure::Error> {
             gl.Clear(gl::COLOR_BUFFER_BIT);
         }
 
-        shader_program.set_used();
-        unsafe {
-            gl.BindVertexArray(vao);
-            gl.DrawArrays(
-                gl::TRIANGLES, // mode
-                0,             // starting index in the enabled arrays
-                3,             // number of indices to be rendered
-            );
-        }
+        let triangle = triangle::Triangle::new(&res, &gl)?;
+        triangle.render(&gl);
 
         window.gl_swap_window();
     }
