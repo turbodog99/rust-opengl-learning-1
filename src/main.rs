b22a180873d5fbfb782378b2a504f11e6b4247ce
Mark Herman, II <mark@exceptionaltech.com>
2019-11-01 10:57:13 -0500

    Uses failure package for error formatting

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
+#[macro_use]
+extern crate failure;
+
 extern crate gl;
 extern crate sdl2;
 
 pub mod render_gl;
 pub mod resources;
 
+use failure::err_msg;
 use resources::Resources;
 use std::path::Path;
 
 fn main() {
-    let sdl = sdl2::init().unwrap();
+    if let Err(e) = run() {
+        println!("{}", failure_to_string(e));
+    }
+}
+
+fn run() -> Result<(), failure::Error> {
+    let sdl = sdl2::init().map_err(err_msg)?;
     let video_subsystem = sdl.video().unwrap();
     let gl_attr = video_subsystem.gl_attr();
 
@@ -34,7 +44,7 @@ fn main() {
         gl.ClearColor(0.3, 0.3, 0.5, 1.0);
     }
 
-    let res = Resources::from_relative_exe_path(Path::new("assets")).unwrap();
+    let res = Resources::from_relative_exe_path(Path::new("assets"))?;
 
     let shader_program = render_gl::Program::from_res(&gl, &res, "shaders/triangle").unwrap();
 
@@ -122,4 +132,37 @@ fn main() {
 
         window.gl_swap_window();
     }
+
+    Ok(())
+}
+
+pub fn failure_to_string(e: failure::Error) -> String {
+    use std::fmt::Write;
+
+    let mut result = String::new();
+
+    for (i, cause) in e
+        .iter_chain()
+        .collect::<Vec<_>>()
+        .into_iter()
+        .rev()
+        .enumerate()
+    {
+        if i > 0 {
+            let _ = writeln!(&mut result, "   Which caused the following issue:");
+        }
+        let _ = write!(&mut result, "{}", cause);
+        if let Some(backtrace) = cause.backtrace() {
+            let backtrace_str = format!("{}", backtrace);
+            if backtrace_str.len() > 0 {
+                let _ = writeln!(&mut result, " This happened at {}", backtrace);
+            } else {
+                let _ = writeln!(&mut result);
+            }
+        } else {
+            let _ = writeln!(&mut result);
+        }
+    }
+
+    result
 }
